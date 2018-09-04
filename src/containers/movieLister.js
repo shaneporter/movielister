22aac29ccec56917605959ccd416ae66e61f9e62
Shane Porter <shane@freshclickmedia.com>
2018-09-04 18:20:40 +0100

    no results jsx

--- src/containers/movieLister.js
+++ src/containers/movieLister.js
@@ -9,16 +9,23 @@ class MovieLister extends Component {
   componentDidMount() {
     this.props.fetchData();
   }
+
+  showResults(movies) {
+    if(movies.length) {
+      return movies.map(movie => <Movie key={movie.id} genres={movie.genres} 
+         title={movie.title} posterUrl={movie.poster_url} rating={movie.vote_average} />);
+    } else {
+      return <div>No movies found, try decreasing the minimum rating and/or selected genres.</div>
+    }
+  }
+  
   render() {
     return (
       <div>
         <h1>Movie Lister</h1>
         <GenreList genres={this.props.movieGenres} onChange={this.props.onChangeGenres} />
         <RatingSelector minimumRating={this.props.minimumRating} onChange={this.props.onChangeMinimumRating} />
-        {
-          this.props.movies.filter(movie => movie.isVisible).map(movie => <Movie key={movie.id} genres={movie.genres} 
-             title={movie.title} posterUrl={movie.poster_url} rating={movie.vote_average} />)
-        }
+        {this.showResults(this.props.movies.filter(movie => movie.isVisible))}
       </div>
     );
   }
