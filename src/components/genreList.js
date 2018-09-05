2244ac0d6e7a7193b94882f687318c53cbda5582
Shane Porter <shane@freshclickmedia.com>
2018-09-05 07:34:04 +0100

    converted GenreList to functional component

--- src/components/genreList.js
+++ src/components/genreList.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-// TODO: convert to functional component
-class GenreList extends Component {
-  render() {
-    return (
-      <div>
-        {
-          this.props.genres.map(genre => 
-            <label key={genre.name}>
-              <input type="checkbox" value={genre.id} defaultChecked={genre.isSelected} 
-                onChange={this.props.onChange} />{genre.name}
-            </label>
-          )
-        }
-      </div>
-    );
-  }
+const GenreList = (props) => {
+  return (
+    <div>
+      {
+        props.genres.map(genre => 
+          <label key={genre.name}>
+            <input type="checkbox" value={genre.id} defaultChecked={genre.isSelected} 
+              onChange={props.onChange} />{genre.name}
+          </label>
+        )
+      }
+    </div>
+  )
 }
 
 GenreList.propTypes = {
