f12704b190575ac213e9a31ca3950cb337c2d8d0
Shane Porter <shane@freshclickmedia.com>
2018-09-07 08:16:28 +0100

    removed typo detritus

--- src/components/ratingSelector.js
+++ src/components/ratingSelector.js
@@ -6,10 +6,9 @@ class RatingSelector extends Component {
   constructor() {
     super();
     this.state = {
-      ratings: new Array(21 - 0).fill().map((d, i) => (i * 0.5) + 0)
+      ratings: new Array(21).fill().map((d, i) => (i * 0.5) + 0)
     }
   }
-  completd
   render() {
     return (
       <Rating>
